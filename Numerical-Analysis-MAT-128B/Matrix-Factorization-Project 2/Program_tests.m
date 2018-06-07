72e619b8d7e09ab8db011b5fccbe61071017f473
Christopher Wong <cwon@ucdavis.edu>
2018-06-06 17:03:37 -0700

    Add files via upload

---
+++ Numerical-Analysis-MAT-128B/Matrix-Factorization-Project 2/Program_tests.m
@@ -0,0 +1,41 @@
+% Program tests
+% clear
+format long e
+
+% cd 'C:\Users\Christopher\Desktop\MAT 128B\Project 2';
+
+%%% Part (7)
+A = [26,12,-42,-20;12,36,-18,-18;-42,-18,81,33;-20,-18,33,19];
+Part6;
+L
+D
+Noper
+
+%%% Part (8)
+load('A.mat')
+Part6;
+L
+D
+Noper
+
+L(2,1)
+L(5,3)
+L(20,19)
+L(35,19)
+L(53,48)
+L(70,2)
+L(80,37)
+L(85,81)
+L(96,56)
+L(100,99)
+
+D(1,1)
+D(7,7)
+D(10,10)
+D(17,17)
+D(20,20)
+D(35,35)
+D(41,41)
+D(66,66)
+D(83,83)
+D(100,100)
